c3826e364206c86314bf0fd9717874c7ffe355ef
Thaina Zamperlim Segura <pedropeo2010@gmail.com>
2025-02-26 21:48:20 -0300

    Modulo 07

---
+++ ExemplosMod07/src/Cliente.java
@@ -0,0 +1,48 @@
+public class Cliente {
+    private int codigo;
+    private String nome;
+    private String endereco;
+
+    public Cliente() {
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return this.endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void cadastrarEndereco(String endereco) {
+        this.setEndereco(endereco);
+    }
+
+    public void imprimirEndereco() {
+        System.out.println(this.endereco);
+    }
+
+    public String retornarNomeCliente() {
+        return "Endereço do Pedro";
+    }
+
+    public int getValorTotal() {
+        return 20;
+    }
+}
