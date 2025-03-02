96a0e46d0e8812540f397dcc3632ae3daad56b42
Thaina Zamperlim Segura <pedropeo2010@gmail.com>
2025-03-02 15:14:56 -0300

    Tarefa Calculo media

---
+++ CalculoMedia/src/Media.java
@@ -0,0 +1,25 @@
+
+public class Media {
+    public static void main(String[] args) {
+        retornaMedia();
+
+    }
+
+
+    public static void retornaMedia(){
+        double nota1 = 10.0;
+        double nota2 = 5.0;
+        double nota3 = 7.8;
+        double nota4 = 6.5;
+        double media;
+
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.printf("A nota do Aluno é: %.2f %n", media);
+    }
+
+
+
+
+
+}
